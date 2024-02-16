869d7b872c16e1de68213fc81c22b79cc65acb17
gustavoEtec0 <gustavo.santos1259@etec.sp.gov.br>
2024-02-15 22:36:35 -0300

    books class add

--- pw3-aula1/src/App.jsx
+++ pw3-aula1/src/App.jsx
@@ -3,20 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App ">
-      <h1>As cavernas de aço</h1>
-      <p>Isaac Asimov</p>
-      <img src={cavernasAco} alt="capa livro As cavernas de aço de Isaac Asimov" className='App-logo' />
-      <p>As Cavernas de Aço, de Isaac Asimov, é o primeiro romance da consagrada Série dos Robôs,
-          uma das mais populares da ficção científica. A história nasceu de um desafio: Asimov queria
-          provar para seu editor que a ficção científica não era limitada e poderia ser incorporada
-          a qualquer gênero literário, inclusive nos dramas policiais. A obra foi publicada,
-          pela primeira vez, na revista Galaxy Science Fiction, e o sucesso foi tão grande que,
-          um ano depois, se tornou livro. Na trama, Os Mundos Siderais, antigas colônias da Terra,
-          possuem rígidas leis de imigração, e não aceitam mais terráqueos em seus planetas.
-          Para agravar a situação, os robôs estão substituindo grande parte da mão de obra trabalhadora na Terra,
-          levando os humanos a perderem seus empregos e privilégios de cidadãos.
-      </p>
+    <div className="App">
+      <div className="cardbook">      
+        <h1>As cavernas de aço</h1>
+        <p>Isaac Asimov</p>
+        <img src={cavernasAco} alt="capa livro As cavernas de aço de Isaac Asimov" className='App-logo' />
+        <p>As Cavernas de Aço, de Isaac Asimov, é o primeiro romance da consagrada Série dos Robôs,
+            uma das mais populares da ficção científica. A história nasceu de um desafio: Asimov queria
+            provar para seu editor que a ficção científica não era limitada e poderia ser incorporada
+            a qualquer gênero literário, inclusive nos dramas policiais. A obra foi publicada,
+            pela primeira vez, na revista Galaxy Science Fiction, e o sucesso foi tão grande que,
+            um ano depois, se tornou livro. Na trama, Os Mundos Siderais, antigas colônias da Terra,
+            possuem rígidas leis de imigração, e não aceitam mais terráqueos em seus planetas.
+            Para agravar a situação, os robôs estão substituindo grande parte da mão de obra trabalhadora na Terra,
+            levando os humanos a perderem seus empregos e privilégios de cidadãos.
+        </p>
+      </div>
     </div>
   );
 }
